2bd4ccbe50136d09a0e633ab4670aa8b55b1f442
David Woolner <davidwoolner@gmail.com>
2021-09-20 12:10:12 -0400

    Add axios jwt packages

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
