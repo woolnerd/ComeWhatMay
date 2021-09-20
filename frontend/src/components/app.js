614f4d24efb7bc00c010681d0573129c6a9ac0ec
David Woolner <davidwoolner@gmail.com>
2021-09-20 14:17:00 -0400

    Add components nav bar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-import NavBarContainer from "./nav/navbar_container";
+// import NavBarContainer from "./nav/navbar_container";
 
 import LandingPage from "./landing_page/landing_page";
-import LoginFormContainer from "./session/login_form_container";
-import SignupFormContainer from "./session/signup_form_container";
+// import LoginFormContainer from "./session/login_form_container";
+// import SignupFormContainer from "./session/signup_form_container";
 
 const App = () => (
   <Switch>
     <AuthRoute exact path="/" component={LandingPage} />
-    <AuthRoute exact path="/login" component={LoginFormContainer} />
-    <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
+    {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
   </Switch>
 );
 
