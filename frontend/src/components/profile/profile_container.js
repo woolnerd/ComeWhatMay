3d4ea7cb87562ce9533afd024487244417cf940f
David Woolner <davidwoolner@gmail.com>
2021-09-21 15:52:13 -0400

    Add profile show component

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -3,19 +3,21 @@ import { connect } from 'react-redux'
 import { fetchUserProfile, updateUserProfile, deleteUserProfile } from "../../actions/profile_actions"
 import Profile from './profile';
 
-const mSTP = ({entities}) => {
-    debugger
-    return 
-    
-    profile: Object.values(entities.profile.data)
+const mSTP = ({entities, session}, ownProps) => {
+    // debugger
+    return {
+      profileId: Object.keys(entities.profile)[0],
+      profile: entities.profile,
+      currentUserId: session.user.id,
+    };
     
 }
 
 
 const mDTP = dispatch => ({
-    fetchUserProfile: () => dispatch(fetchUserProfile),
-    updateUserProfile: () => dispatch(updateUserProfile),
-    deleteUserProfile: () => dispatch(deleteUserProfile)
+    fetchUserProfile: (userId) => dispatch(fetchUserProfile(userId)),
+    updateUserProfile: (profile) => dispatch(profile),
+    deleteUserProfile: (profileId) => dispatch(profileId)
 })
 
 export default connect(mSTP, mDTP)(Profile)
