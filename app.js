59df9822e997b69ffc52aabdd07c34c5e69567cb
David Woolner <davidwoolner@gmail.com>
2021-09-21 20:57:44 -0400

    Add keys

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const disasterPlans = require("./routes/api/disaster_plans")
 const actionSteps = require("./routes/api/action_steps")
 const relatives = require("./routes/api/relatives")
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => ("Connected to MongoDB successfully"))
