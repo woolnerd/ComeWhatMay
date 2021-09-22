578bc143a0bb287899114a0b55201f0859b915b6
dustin <dustin.adler@yahoo.com>
2021-09-22 13:50:55 -0500

    disaster drill utils, actions, reducer, and window

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -3,9 +3,12 @@ import { combineReducers } from 'redux';
 import profile from './profile_reducer';
 import relative from './relative_reducer';
 import disasterPlan from './disaster_plan_reducer'
+import disasterDrill from './disaster_drill_reducer'
+
 
 export default combineReducers({
     profile,
     relative,
-    disasterPlan
+    disasterPlan,
+    disasterDrill
 })
