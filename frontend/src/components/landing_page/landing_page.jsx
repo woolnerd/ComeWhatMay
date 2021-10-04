2d13902a6467e54c3e0f95a172d419dbbcf6efe8
elsicab <ecaba006@gmail.com>
2021-10-04 19:44:34 -0400

    Homepage styling

--- frontend/src/components/landing_page/landing_page.jsx
+++ frontend/src/components/landing_page/landing_page.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./landing_page.css";
 import { Link } from "react-router-dom";
+import { CgProfile} from "react-icons/cg"
+import { BsPencilSquare} from "react-icons/bs"
+import { AiOutlineFire} from "react-icons/ai"
+import { GiWhistle} from "react-icons/gi"
 
 class LandingPage extends React.Component {
   render() {
@@ -24,6 +28,33 @@ class LandingPage extends React.Component {
           <div className="banner-info">
             <h2>Come What May</h2>
             <h4>The Disaster Preparation App - When the unexpected happens, be prepared.</h4>
+          </div>
+        </div>
+        <div class="container">How <p> CWM </p> Works</div>
+        <div className="app-features">
+          <div className="app-icon-sec">
+            <div className="app-icon"><BsPencilSquare /></div>
+            <h4>Register</h4>
+            <p class="icon-num">- 1 -</p>
+            <p class="icon-info">Have access to all features.</p>
+          </div>
+          <div className="app-icon-sec">
+            <div className="app-icon"><CgProfile /></div>
+            <h4>Create a Profile</h4>
+            <p class="icon-num">- 2 -</p>
+            <p class="icon-info">Add household information to keep track of your loved ones and have a plan in place.</p>
+          </div>
+          <div className="app-icon-sec">
+            <div className="app-icon"><AiOutlineFire /></div>
+            <h4>Create a Plan</h4>
+            <p class="icon-num">- 3 -</p>
+            <p class="icon-info">Personalize emergency plans for all situations.</p>
+          </div>
+          <div className="app-icon-sec">
+            <div className="app-icon"><GiWhistle /></div>
+            <h4>Practice</h4>
+            <p class="icon-num">- 4 -</p>
+            <p class="icon-info">Put your preparation to the test! Run live drills and keep track of your history.</p>
           </div>
         </div>
       </div>
